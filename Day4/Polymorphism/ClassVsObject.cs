be01de32d7dadba9c73620147fd7bb385fb3ce93
Gautami Pawar <gautamipawar19@gmail.com>
2026-02-07 16:31:28 +0530

    Delete Day4/Polymorphism directory

--- Day4/Polymorphism/ClassVsObject.cs
+++
@@ -1,9 +0,0 @@
-public class ClassVsObject
-{
-    public int MyProperty {get; set;}
-
-    public ClassVsObject()
-    {
-        MyProperty = 10;
-    }
-}
--- Day4/Polymorphism/StaticClassDemo.cs
+++
@@ -1,8 +0,0 @@
-static class StaticClassDemo
-{
-    public static int StatiClassDemo =10;
-    public static void Display()
-    {
-        Console.WriteLine("This is a static class Demo");
-    }
-}
