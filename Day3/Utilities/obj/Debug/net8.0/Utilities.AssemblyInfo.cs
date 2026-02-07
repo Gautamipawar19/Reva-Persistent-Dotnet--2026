63fcca88b539b4878500bee28416a97c33a10124
Gautamipawar19 <gautamipawar19@gmail.com>
2026-02-07 16:33:23 +0530

    Methods&Members

--- Day3/Utilities/obj/Debug/net8.0/Utilities.AssemblyInfo.cs
+++ Day3/Utilities/obj/Debug/net8.0/Utilities.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Utilities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5500a08f72e8ce0f36aa3de0c08d19226bf1e02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03c883d1c93704393f1a6b4ffc08f54da4c71db5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Utilities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Utilities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
