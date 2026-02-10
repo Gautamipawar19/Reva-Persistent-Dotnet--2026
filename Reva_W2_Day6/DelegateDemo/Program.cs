0a69943b5af22828bcf90ce624723e5808800e2f
Gautamipawar19 <gautamipawar19@gmail.com>
2026-02-10 13:00:41 +0530

    Delegate Demo

--- Reva_W2_Day6/DelegateDemo/Program.cs
+++ Reva_W2_Day6/DelegateDemo/Program.cs
@@ -17,19 +17,52 @@ namespace DelegateDemo
 class DelegatesDemo
 {
     //void Add(int a, int b)
-    delegate void MathOperation(int a, int b);
+    delegate int  MathOperation(int a, int b);
     public void Run(){
-       MathOperation operation = Subtract; //Add or Subtract;
-       operation(5, 3);   
+       MathOperation operation = Add; //Add or Subtract;
+        
+        //Multicas delegate : adding more methods to the inovation
+       operation +=Substract;
+       operation +=Multiply;
+       operation +=Divide;
+
+        operation -= Substract; //Removing a method from the invocation list
+
+       var result = operation(5, 3);
+       Console.WriteLine($"The final result is: {result}");
+
+        
     }
 
-    public void Add(int a, int b)
+    public int Add(int a, int b)
     {
         Console.WriteLine($"The Sum of {a} and {b} is: {a + b}");
+        return a + b;
     }
 
-    public void Subtract(int a, int b)
+    public int Substract(int a, int b)
     {
         Console.WriteLine($"The difference between {a} and {b} is: {a - b}");
+        return a - b;
+    }
+
+    public int Multiply(int a, int b)
+    {
+        Console.WriteLine($"The product of {a} and {b} is: {a * b}");
+        return a * b;
+    }
+    
+    public int Divide(int a, int b){
+        if(b != 0)
+        {
+            Console.WriteLine($"The quotient of {a} and {b} is: {a / b}");
+            return a / b;
+        }
+        else
+        {
+            Console.WriteLine("Cannot divide by zero.");
+        }
+        return 0;
+
     }
 }
