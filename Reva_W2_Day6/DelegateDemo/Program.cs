346ebdac86b3cb1eb5d2d641232e30ebda755807
Gautamipawar19 <gautamipawar19@gmail.com>
2026-02-10 10:48:21 +0530

    Function Pointer Example

---
+++ Reva_W2_Day6/DelegateDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace DelegateDemo
+{
+    class Program
+    {
+         static void Main(String[] args)
+        {
+            DelegatesDemo app = new DelegatesDemo();
+            app.Run();
+           
+        }
+    }
+}
+
+class DelegatesDemo
+{
+    //void Add(int a, int b)
+    delegate void MathOperation(int a, int b);
+    public void Run(){
+       MathOperation operation = Subtract; //Add or Subtract;
+       operation(5, 3);   
+    }
+
+    public void Add(int a, int b)
+    {
+        Console.WriteLine($"The Sum of {a} and {b} is: {a + b}");
+    }
+
+    public void Subtract(int a, int b)
+    {
+        Console.WriteLine($"The difference between {a} and {b} is: {a - b}");
+    }
+}
